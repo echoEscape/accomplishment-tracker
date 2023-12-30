9ed609a450970cf2f9a23ddaafbe7026e779182d
echoEscape <thenumberiszero@gmail.com>
2023-12-30 23:45:48 +0100

    Implement Component into main app. Understanding structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  //templateUrl: './app.component.html',
+  template: `<app-header></app-header>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
