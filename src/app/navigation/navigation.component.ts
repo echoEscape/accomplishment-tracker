3ff35161a4a07c004f777c064f5db72986b84225
echoEscape <thenumberiszero@gmail.com>
2024-01-05 21:22:08 +0100

    Implement ngClass to toggle CSS Classes on click

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,15 +1,18 @@
-import { AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {Component} from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })
-export class NavigationComponent {
-  switchNavigationSize(){
-    
-  }
 
+export class NavigationComponent {
+  public toggledNav = false;
+  
+  public toggleNavSize(){
+    this.toggledNav = !this.toggledNav;
+  }
 }
